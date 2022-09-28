80fbaf33ef78e394ac1278e2dd57154bc2b0390b
David Robertson <davidr@element.io>
2022-09-28 21:44:16 +0100

    chekpoint

---
+++ tests/playground/test_mro_caching.py
@@ -0,0 +1,45 @@
+import shutil
+
+from mypy import options, build
+from mypy.fscache import FileSystemCache
+from mypy.modulefinder import BuildSource
+
+SOURCE_CODE = """
+from twisted.internet import protocol
+
+class Foo(protocol.Protocol):
+    pass
+"""
+
+opts = options.Options()
+opts.show_traceback = True
+opts.namespace_packages = True
+opts.mypy_path = ["tests/playground/site-packages"]
+opts.ignore_missing_imports = True
+opts.ignore_missing_imports_per_module = True
+opts.plugins = ['mypy_zope:plugin']
+# Config file is needed to load plugins, it doesn't not exist and is not
+# supposed to.
+opts.config_file = '    not_existing_config.ini'
+
+shutil.rmtree(".mypy_cache_dmr/3.10/twisted", ignore_errors=True)
+
+source = BuildSource(None,
+                     module=None,
+                     text=SOURCE_CODE,
+                     base_dir=None)
+cache = FileSystemCache()
+results = []
+for i in range(2):
+    result = build.build(
+        sources=[source],
+        options=opts,
+        # alt_lib_path="tests/playground/site-packages",
+        fscache=cache,
+    )
+
+    print(result.errors)
+    results.append(result)
+
+assert not results[0].errors, results[1].errors
+assert not results[1].errors, results[1].errors
